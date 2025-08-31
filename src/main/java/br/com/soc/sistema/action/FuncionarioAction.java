1a12d2b57ae9a7f2cda5be3aa6f2e5a5b047e2f8
AnnaBeatrizds <annabeatriz.oliveirarib@gmail.com>
2025-08-31 06:32:24 -0300

    Implementa CRUD de funcionários com correção problema de codificação UTF-8

---
+++ src/main/java/br/com/soc/sistema/action/FuncionarioAction.java
@@ -0,0 +1,106 @@
+package br.com.soc.sistema.action;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import br.com.soc.sistema.business.FuncionarioBusiness;
+import br.com.soc.sistema.filter.FuncionarioFilter;
+import br.com.soc.sistema.infra.Action;
+import br.com.soc.sistema.infra.OpcoesComboBuscar;
+import br.com.soc.sistema.vo.FuncionarioVo;
+
+public class FuncionarioAction extends Action {
+	
+	private List<FuncionarioVo> funcionarios = new ArrayList<>();
+	private FuncionarioBusiness business = new FuncionarioBusiness();
+	private FuncionarioFilter filtrar = new FuncionarioFilter();
+	private FuncionarioVo funcionarioVo = new FuncionarioVo();
+	private String rowid;
+	
+	public String todos() {
+		funcionarios.addAll(business.trazerTodosOsFuncionarios());	
+
+		return SUCCESS;
+	}
+	
+	public String filtrar() {
+		if(filtrar.isNullOpcoesCombo())
+			return REDIRECT;
+		
+		funcionarios = business.filtrarFuncionarios(filtrar);
+		
+		return SUCCESS;
+	}
+	
+	public String novo() {
+		if(funcionarioVo.getNome() == null)
+			return INPUT;
+		
+		business.salvarFuncionario(funcionarioVo);
+		
+		return REDIRECT;
+	}
+	
+	public String editar() {
+		if(funcionarioVo.getRowid() == null)
+			return REDIRECT;
+		
+		funcionarioVo = business.buscarFuncionarioPor(funcionarioVo.getRowid());
+		
+		return INPUT;
+	}
+	public String alterar() {
+	    try {
+	 
+	        business.alterarFuncionario(funcionarioVo);
+	        return "redirect";
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return "input";
+	    }
+	}
+	public String excluir() {
+	    try {
+	        System.out.println("Recebendo ID para exclusão: " + this.rowid);
+	        
+	        business.excluirFuncionario(this.rowid);
+	        return "success";
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return "error";
+	    }
+	}
+	public void setRowid(String rowid) {
+	    this.rowid = rowid;
+	}
+	public List<OpcoesComboBuscar> getListaOpcoesCombo(){
+		return Arrays.asList(OpcoesComboBuscar.values());
+	}
+	
+	public List<FuncionarioVo> getFuncionarios() {
+		return funcionarios;
+	}
+
+	public void setFuncionarios(List<FuncionarioVo> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
+
+	public FuncionarioFilter getFiltrar() {
+		return filtrar;
+	}
+
+	public void setFiltrar(FuncionarioFilter filtrar) {
+		this.filtrar = filtrar;
+	}
+
+	public FuncionarioVo getFuncionarioVo() {
+		return funcionarioVo;
+	}
+
+	public void setFuncionarioVo(FuncionarioVo funcionarioVo) {
+		this.funcionarioVo = funcionarioVo;
+	}
+}
---
+++ src/main/webapp/funcionario/cadFuncionario_i.jsp
@@ -0,0 +1,70 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF8">
+		<title><s:text name="label.titulo.pagina.cadastro"/></title>
+		<link rel='stylesheet' href='webjars/bootstrap/5.1.3/css/bootstrap.min.css'>
+	</head>
+	<body class="bg-secondary">
+
+		<div class="container">
+			<s:url action="%{funcionarioVo.rowid != null ? 'alterarFuncionarios' : 'novoFuncionarios'}" var="formAction"/>
+			<s:form action="%{formAction}">
+
+				<div class="card mt-5">
+					<div class="card-header">
+						<div class="row">
+							<div class="col-sm-5">
+								<s:url action="todosFuncionarios" var="todos"/>
+								<a href="${todos}" class="btn btn-success" >Funcionários</a>
+							</div>
+							
+							<div class="col-sm">
+								<s:if test="funcionarioVo.rowid != null">
+									<h5 class="card-title">Editar Funcionário</h5>
+								</s:if>
+								<s:else>
+									<h5 class="card-title">Novo Funcionário</h5>
+								</s:else>
+							</div>
+						</div>
+					</div>
+					
+					<div class="card-body">
+						<div class="row align-items-center">
+							<label for="id" class="col-sm-1 col-form-label text-center">
+								Código:
+							</label>	
+
+							<div class="col-sm-2">
+								<s:textfield cssClass="form-control" id="id" value="%{funcionarioVo.rowid}" readonly="true"/>
+								<s:hidden name="funcionarioVo.rowid" />
+							</div>	
+						</div>
+						
+						<div class="row align-items-center mt-3">
+							<label for="nome" class="col-sm-1 col-form-label text-center">
+								Nome:
+							</label>	
+
+							<div class="col-sm-5">
+								<s:textfield cssClass="form-control" id="nome" name="funcionarioVo.nome"/>							
+							</div>	
+						</div>
+					</div>
+
+					<div class="card-footer">
+						<div class="form-row">
+							<s:submit value="Salvar" cssClass="btn btn-primary col-sm-4 offset-sm-1"/>
+							<button type="reset" class="btn btn-secondary col-sm-4 offset-sm-2">Limpar Formulario</button>
+						</div>
+					</div>
+				</div>
+			</s:form>			
+		</div>
+		
+		<script src="webjars/bootstrap/5.1.3/js/bootstrap.bundle.min.js"></script>
+	</body>
+</html>
